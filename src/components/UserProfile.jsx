7d08f6b17d667b8500602a3b3b80db21ab26756d
vinod-ayckattu <vinodharidas.dev24@gmail.com>
2025-11-25 10:51:15 +0530

    influencer-update

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -7,8 +7,10 @@ export default function UserProfile() {
   const { id } = useParams();
   const [user, setUser] = useState({ posts: [] });
   const { followInfluencer } = useContext(GlobalContext);
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
+    //setMessage('');
     axios.get(`http://127.0.0.1:8000/api/user/${id}`)
       .then(res => setUser(res.data.user))
       .catch(err => console.error(err));
@@ -17,7 +19,11 @@ export default function UserProfile() {
   return (
         <div className='container d-flex justify-content-center w-100'>
                 <div className="text-start">
-                <div className="d-flex justify-content-between mt-3"><h3>{user.name}</h3><button onClick={followInfluencer(user.id)} class="btn btn-sm btn-success rounded-0 fw-bold">Follow</button></div>
+                  <p className="text-primary">{message}</p>
+                <div className="d-flex justify-content-between mt-3">
+                  <h3>{user.name}</h3>
+                  <button onClick={() => { setMessage(followInfluencer(user.id));}} className="btn btn-sm btn-success rounded-0 fw-bold">Follow</button>
+                </div>
                 {user.posts.map((post) => (
                     <div key={post.id} className="border rounded p-2 my-2">
                     {post.post}
--- src/context/GlobalProvider.jsx
+++ src/context/GlobalProvider.jsx
@@ -1,16 +1,21 @@
 import axios from "axios";
 import { GlobalContext } from "./GlobalContext";
+import { useState } from "react";
 
 export default function GlobalProvider({ children }) {
 
     const token = localStorage.getItem('sl-api-token');
+    const [message, setMessage] = useState('');
+
     const followInfluencer = (creatorId) => {
-    
+    setMessage('');
     axios.post("http://127.0.0.1:8000/api/follow", {'creator_id': creatorId}, 
             { headers: { Authorization: `Bearer ${token}` }
-            }).finally((res)=> {
-                console.log(res.data.message);
+            }).then((res)=> {
+                //console.log(res);
+                setMessage(res.data.message);
             });
+    return message;
     }
     return (
         <GlobalContext.Provider value={{ followInfluencer }}>
